3194009b1ae0ef05f874bb0761a0b9125d45b8e8
brodieG <brodieG@users.noreply.github.com>
2017-09-09 19:22:41 -0400

    add tests for eval, fix recsub ones

---
+++ tests/unitizer/eval.R
@@ -0,0 +1,27 @@
+library(recsub)
+
+unitizer_sect("basic eval", {
+  a <- quote(x > 5)
+  b <- quote(x < 10)
+  x <- 9:10
+
+  eval_r(quote(x[a & b]))
+
+  ## Add an interceding frame; here we want to ensure that the
+  ## variable used is the one in the data.frame, not the one we
+  ## just defined above.
+  DF <- data.frame(x=5:7, y=letters[1:3], stringsAsFactors=FALSE)
+  DF$x    # use this one
+  x       # not this one
+  eval_r(quote(DF[a & b, ,drop=FALSE]))            # incorrect
+  eval_r(quote(DF[a & b, ,drop=FALSE]), envir=DF)  # correct
+})
+unitizer_sect("subset2", {
+  ## Implement programmable NSE in a function; use `substitute` to capture
+  ## input unevaluated
+  subset2 <- function(x, subset) {
+    sub.val <- eval_r(substitute(subset), envir=x, enclos=parent.frame())
+    x[!is.na(sub.val) & sub.val, ]
+  }
+  subset2(DF, a & y < 'c')
+})
--- tests/unitizer/recsub.R
+++ tests/unitizer/recsub.R
@@ -1,10 +1,18 @@
-library(subversive)
+library(recsub)
+
 unitizer_sect("simple tests", {
   a <- quote(x > 3)
   b <- quote(x < 10)
+
+  recsub(quote(a & b))
+
   c <- quote(a & b)
 
   recsub(c)
+
+  l <- list(b=quote(x < 1e4), d=quote(b))
+
+  recsub(c, l)
 })
 unitizer_sect("catch inf rec", {
   a1 <- quote(b1 > 3)
